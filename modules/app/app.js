b97ba02153655d89373291f1428af3b5da4c8e2a
Desmond Wareham <desmond.wareham@gmail.com>
2020-06-24 14:42:55 -0400

    most recent

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 
-app.use('/people', require('../people/people.router'));
-app.use('/pets', require('../pets/pets.router'));
+app.use("/people", require("../people/people.router"));
+app.use("/pets", require("../pets/pets.router"));
 
 module.exports = app;
