a9218351ba2d6714d3269a4f7e3879e60d71b240
Desmond Wareham <desmond.wareham@gmail.com>
2020-06-30 13:07:05 -0400

    latest

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 
-const cors = require("cors");
+// const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
 app.use("/people", require("../people/people.router"));
 app.use("/pets", require("../pets/pets.router"));
--- modules/people/people.router.js
+++ modules/people/people.router.js
@@ -6,15 +6,22 @@ const People = require('./people.service');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+ 
   // Return all the people currently in the queue.
   const people=People.get();
+  
   return res.status(200).json(people);
 });
 
-router.post('/', json, (req, res) => {
+router.use(json)
+
+router.post('/',(req, res) => {
+ 
   // Add a new person to the queue.
   const {name}=req.body;
+  console.log(name);
   People.enqueue(name);
+  console.log(name);
   return res.status(201).end;
 });
 
