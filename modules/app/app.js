3b0a0e7aa53b880aa840bc4281c25e6c5c833a33
Desmond Wareham <desmond.wareham@gmail.com>
2020-07-17 11:32:23 -0400

    cors

--- modules/app/app.js
+++ modules/app/app.js
@@ -2,15 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
-const { CLIENT_ORIGIN, NODE_ENV } = require('../../config')
+const { NODE_ENV } = require('../../config')
 const app = express()
 
 
-app.use(cors({
-  origin: CLIENT_ORIGIN
-}));
+// app.use(cors({
+//   origin: CLIENT_ORIGIN
+// }));
 
-// app.use(cors());
+app.use(cors());
 
 app.use('/people', require('../people/people.router'))
 app.use('/pets/cat', require('../pets/cats.router'))
